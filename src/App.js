840c0fed406f9c82811e6f8c1e0ccf815d11c3b6
Peter Xervi <xervipeter@gmail.com>
2025-10-20 07:56:04 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+// App.js
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Failed to fetch users:", error));
+  }, []);
+
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      {/* Pass users to nested routes */}
+      <Outlet context={users} />
     </>
   );
-};
+}
 
 export default App;
